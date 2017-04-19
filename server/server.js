cb3a4676426fdaab7b37ce28c899a38107d21d81
Mike Peritz <mike.peritz@gmail.com>
2017-04-19 17:45:29 -0400

    added webpack and react - app is operational

--- server/server.js
+++ server/server.js
@@ -15,3 +15,15 @@ app.use(morgan('dev')); // logging
 app.use(express.static(resolve(__dirname, '..', 'public'))); // static files
 
 app.use('/api', require('./routes')); // matches all requests to /api
+
+app.get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')));
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(1337, function () {
+  console.log('Your server, listening on port 1337');
+});
